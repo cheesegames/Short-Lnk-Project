8d780971ad2e87496661fbaef05b13046697fb73
Ben Sampson <ben@cheesegames.net>
2017-04-14 11:52:55 +1000

    Fixed login page

--- imports/ui/Login.jsx
+++ imports/ui/Login.jsx
@@ -24,7 +24,7 @@ export default class Login extends Component {
     });
   }
   render() {
-    return ( 
+    return (
       <div className="boxed-view">
         <div className="boxed-view__box">
           <h1>login to short lnk</h1>
